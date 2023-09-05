192d2808be21445375d362255c671141e6adeeab
rami <rami_abuzaid@hotmail.com>
2023-09-05 19:30:59 +0200

    register page

--- app/login.js
+++ app/login.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [infoData, setInfoData] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
   const navigation = useNavigation();
 
@@ -15,46 +18,49 @@ export default function Login() {
     infoData.message === "Successfully authenticated";
 
   const submittedData = async () => {
-    const loginInfo = {
-      username,
-      password,
-    };
+    try {
+      const loginInfo = {
+        username,
+        password,
+      };
 
-    const response = await axios.post(
-      "https://chat-api-with-auth.up.railway.app/auth/token",
-      loginInfo
-    );
-    const data = await response.data;
-    console.log(data)
-    if (data) setInfoData(data);
-    if(checkIfAuth) navigation.navigate("chat")
+      const response = await axios.post(
+        "https://chat-api-with-auth.up.railway.app/auth/token",
+        loginInfo
+      );
+      const data = await response.data;
+      console.log(data);
+      if (data) {
+        setInfoData(data);
+        await AsyncStorage.setItem("user", JSON.stringify(data));
+      }
+      if (checkIfAuth) navigation.navigate("chat");
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
   };
 
   return (
-          <View
-            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-          >
-            <Text style={styles.textHeader}>
-              {infoData.message ?? "Enter username and password"}
-            </Text>
-            <TextInput
-              type="text"
-              placeholder="Username"
-              style={styles.input}
-              value={username}
-              onChangeText={setUsername}
-            />
-            <TextInput
-              type="password"
-              placeholder="Password"
-              style={styles.input}
-              value={password}
-              onChangeText={setPassword}
-            />
-            <Pressable style={styles.button} onPress={submittedData}>
-              <Text style={styles.text}>Login</Text>
-            </Pressable>
-          </View>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={styles.textHeader}>{errorMessage ? errorMessage : ""}</Text>
+      <TextInput
+        type="text"
+        placeholder="Username"
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Password"
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Pressable style={styles.button} onPress={submittedData}>
+        <Text style={styles.text}>Login</Text>
+      </Pressable>
+    </View>
   );
 }
 
@@ -79,7 +85,8 @@ const styles = StyleSheet.create({
   },
   textHeader: {
     fontSize: 15,
-    color: "black",
+    color: "red",
+    fontWeight: "bold",
     padding: 10,
     height: 50,
   },
--- app/register.js
+++ app/register.js
@@ -1,11 +1,93 @@
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
+import axios from 'axios';
+
 
 export default function Register(){
+    const [newUsername, setNewUsername] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [infoData, setInfoData] = useState({});
+    const [errorMessage, setErrorMessage] = useState("");
+  
+    const navigation = useNavigation();
+  
+    const checkIfRegistered = () =>
+      infoData.status === 200 &&
+      infoData.message === "Successfully registered";
+  
+    const submittedData = async () => {
+      try {
+        const registerInfo = {
+            username: newUsername,
+            password: newPassword,
+        };
+  
+        const response = await axios.post(
+          "https://chat-api-with-auth.up.railway.app/auth/register",
+          registerInfo
+        );
+        const data = await response.data;
+        if (data) setInfoData(data);
+        if (checkIfRegistered) navigation.navigate("login");
+      } catch (error) {
+        setErrorMessage(error.response.data.message);
+      }
+    };
     return(
-        <View>
-            <Text>
-                register
-            </Text>
-        </View>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={styles.textHeader}>{errorMessage ? errorMessage : ""}</Text>
+        <TextInput
+          type="text"
+          placeholder="Username"
+          style={styles.input}
+          value={newUsername}
+          onChangeText={setNewUsername}
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+          style={styles.input}
+          value={newPassword}
+          onChangeText={setNewPassword}
+        />
+        <Pressable style={styles.button} onPress={submittedData}>
+          <Text style={styles.text}>Register</Text>
+        </Pressable>
+      </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      width: 250,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+    button: {
+      width: 250,
+      height: 45,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: "black",
+    },
+    textHeader: {
+      fontSize: 15,
+      color: "red",
+      fontWeight: "bold",
+      padding: 10,
+      height: 50,
+    },
+    text: {
+      fontSize: 15,
+      fontWeight: "bold",
+      color: "white",
+    },
+  });
+  
